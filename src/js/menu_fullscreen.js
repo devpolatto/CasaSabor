71f66d4f8a217c0caa3ff150a479649ea67b424e
devpolatto <polatto.xcode.job@gmail.com>
2019-07-19 23:28:25 -0300

    primero commit

---
+++ src/js/menu_fullscreen.js
@@ -0,0 +1,8 @@
+/*executa quando clica no elemento span */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+/*executa quando clica no no elemento "x" dentro do div overlay*/
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
